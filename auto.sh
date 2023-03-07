c8d5d2bc46a97f3a440c85ca5bcbabf0f3d01ebc
sunitsave@gmail.com <sunitsave@gmail.com>
2023-03-07 19:25:47 +0530

    Automation-v0.2

--- auto.sh
+++ auto.sh
@@ -1,3 +1,6 @@
+#############################################################################
+#####Automation-v0.1
+
 dev_name="Sunit"
 s3_bucket_name="upgrad-sunit"
 
@@ -39,3 +42,30 @@ else
     sudo apt install awscli -y
 fi
 aws s3 cp /tmp/${dev_name}-httpd-logs-${process_timestamp}.tar s3://${s3_bucket_name}/${dev_name}-httpd-logs-${process_timestamp}.tar
+
+
+
+#############################################################################
+###########Automation-v0.2
+
+
+if [  -f $automate_cron_job ]
+then
+    echo "cron exist"
+else
+echo "creating a cron job"
+    printf "0 0 * * * root /home/ubuntu/auto/auto.sh \n" > $automate_cron_job
+fi
+
+
+
+inv_file_location="/var/www/html/inventory.html"
+tar_file_size=$(ls -lh /tmp/${dev_name}-httpd-logs-${process_timestamp}.tar | awk '{ print $5}')
+
+if [ -e $inv_file_location ]
+then
+echo "httpd-logs &nbsp;&nbsp;"$process_timestamp"&nbsp;&nbsp;TAR&nbsp;&nbsp;"$tar_file_size"<br/>" >> $inv_file_location
+else
+echo "<b>Log Type&nbsp;&nbsp;Date Created&nbsp;&nbsp;Type&nbsp;&nbsp;Size</b><br/>"  >> $inv_file_location
+echo "httpd-logs &nbsp;&nbsp;"$process_timestamp"&nbsp;&nbsp;TAR&nbsp;&nbsp;"$tar_file_size"<br/>" >> $inv_file_location
+fi
